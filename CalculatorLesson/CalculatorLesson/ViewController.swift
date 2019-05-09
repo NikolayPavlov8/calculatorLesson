a59b23be54c7355b4ea7aef7e4ca141601000536
NikolayPavlov8 <nikolaspavlov8@gmail.com>
2019-05-09 15:03:38 +0300

    code5

--- CalculatorLesson/CalculatorLesson/ViewController.swift
+++ CalculatorLesson/CalculatorLesson/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var result: UILabel!
+    
+    @IBAction func digits(_ sender: UIButton) {
+        result.text = result.text! + String(sender.tag)
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
